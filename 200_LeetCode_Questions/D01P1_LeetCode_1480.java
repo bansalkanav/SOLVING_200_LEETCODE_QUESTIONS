87a77fa9606b90bbdf09d47216b137783ac095a8
Satyam Bhatia <bhatiasatyam1725@gmail.com>
2020-08-27 23:35:39 +0530

    Update D01P1_LeetCode_1480.java

--- 200_LeetCode_Questions/D01P1_LeetCode_1480.java
+++ 200_LeetCode_Questions/D01P1_LeetCode_1480.java
@@ -1,1 +1,13 @@
-# Java code coming soon
+//Runtime : 0ms faster than 100% of Java Submissions
+//Memory usage : 39 MB less than 99.58% of Java Submissions 
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] running_sum = new int[nums.length];
+        running_sum[0] = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            running_sum[i] = running_sum[i - 1] + nums[i];
+        }
+        return running_sum;
+    }
+}
