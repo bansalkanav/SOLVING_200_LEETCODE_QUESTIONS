964d2ca5a988bcfc0e91b72403338137cffe5b06
Satyam Bhatia <bhatiasatyam1725@gmail.com>
2020-08-27 23:42:08 +0530

    Update D02P3_LeetCode_1512.java

--- 200_LeetCode_Questions/D02P3_LeetCode_1512.java
+++ 200_LeetCode_Questions/D02P3_LeetCode_1512.java
@@ -1,1 +1,16 @@
-# Java code coming soon
+//Runtime: 0 ms, faster than 100.00% of Java online submissions for Number of Good Pairs.
+//Memory Usage: 36.8 MB, less than 79.98% of Java online submissions for Number of Good Pairs.
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer, Integer> frequency = new HashMap<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            frequency.put(nums[i], frequency.getOrDefault(nums[i], 0) + 1);
+        }
+        int pairs = 0;
+        for (int v : frequency.values()) {
+            pairs += v * (v - 1)/2;
+        }
+        return pairs;
+    }
+}
