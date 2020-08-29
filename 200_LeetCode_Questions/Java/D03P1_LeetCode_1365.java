11a724d16c9d75300bb16fb87e3270464ac78a4b
Satyam Bhatia <bhatiasatyam1725@gmail.com>
2020-08-30 00:14:47 +0530

    Update D03P1_LeetCode_1365.java

--- 200_LeetCode_Questions/Java/D03P1_LeetCode_1365.java
+++ 200_LeetCode_Questions/Java/D03P1_LeetCode_1365.java
@@ -1,1 +1,23 @@
-# Java code coming soon
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for How Many Numbers Are Smaller Than the Current Number.
+// Memory Usage: 39.5 MB, less than 83.12% of Java online submissions for How Many Numbers Are Smaller Than the Current Number.
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] freq = new int[101]; 
+        for (int i = 0; i < nums.length; i++) {
+                freq[nums[i]]++;
+        }
+        int count = 0;
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != 0) { 
+                int f = freq[i];
+                freq[i] = count;
+                count = count + f;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = freq[nums[i]];
+        }
+        return nums;
+    }
+}
