1ca87b7576589c1f4344b9316150c526ccac3028
Satyam Bhatia <bhatiasatyam1725@gmail.com>
2020-08-30 00:30:14 +0530

    Update D04P2_LeetCode_1395.java

--- 200_LeetCode_Questions/Java/D04P2_LeetCode_1395.java
+++ 200_LeetCode_Questions/Java/D04P2_LeetCode_1395.java
@@ -1,1 +1,24 @@
-# Java code coming soon
+// Runtime: 3 ms, faster than 75.45% of Java online submissions for Count Number of Teams.
+// Memory Usage: 37.1 MB, less than 67.41% of Java online submissions for Count Number of Teams.
+
+class Solution {
+    public int numTeams(int[] rating) {
+        int teams = 0;
+        int[] low;
+        int[] high;
+        for (int i = 1; i < rating.length - 1; ++i) {
+            low = new int[2];
+            high = new int[2];
+            for (int j = 0; j < rating.length; ++j) {
+                if (rating[i] < rating[j]){
+                    ++low[j > i ? 1 : 0];
+                }
+                if (rating[i] > rating[j]){
+                    ++high[j > i ? 1 : 0];
+                }
+            }
+            teams += low[0] * high[1] + high[0] * low[1];
+        }
+        return teams;
+    }
+}
