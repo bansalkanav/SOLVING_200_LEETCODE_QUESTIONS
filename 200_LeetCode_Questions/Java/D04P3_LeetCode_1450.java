5b174e17c1044437f6ffb1f7fa4c4798ea966684
Satyam Bhatia <bhatiasatyam1725@gmail.com>
2020-08-30 00:34:38 +0530

    Update D04P3_LeetCode_1450.java

--- 200_LeetCode_Questions/Java/D04P3_LeetCode_1450.java
+++ 200_LeetCode_Questions/Java/D04P3_LeetCode_1450.java
@@ -1,1 +1,14 @@
-# Java code coming soon
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Number of Students Doing Homework at a Given Time.
+// Memory Usage: 37.9 MB, less than 42.26% of Java online submissions for Number of Students Doing Homework at a Given Time.
+
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int students = 0;
+        for(int i = 0; i < startTime.length; i++){
+            if(startTime[i] <= queryTime && endTime[i] >= queryTime){
+                students++;
+            }
+        }
+        return students;
+    }
+}
