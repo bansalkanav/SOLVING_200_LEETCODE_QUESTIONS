2edee3942a4e86ff6cdc1d1c4dcc8c265e0ec741
Satyam Bhatia <bhatiasatyam1725@gmail.com>
2020-08-30 00:20:44 +0530

    Update D03P3_LeetCode_1486.java

--- 200_LeetCode_Questions/Java/D03P3_LeetCode_1486.java
+++ 200_LeetCode_Questions/Java/D03P3_LeetCode_1486.java
@@ -1,1 +1,12 @@
-# Java code coming soon
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for XOR Operation in an Array.
+// Memory Usage: 36.4 MB, less than 55.35% of Java online submissions for XOR Operation in an Array.
+
+class Solution {
+    public int xorOperation(int n, int start) {
+        int xor = 0;
+        for(int i = 0; i < n; i++){
+            xor ^= (start + (2 * i));
+        }
+        return xor;
+    }
+}
