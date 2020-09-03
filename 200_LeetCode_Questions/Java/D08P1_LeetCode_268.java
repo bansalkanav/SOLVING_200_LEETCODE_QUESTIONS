49d6c419e3be3c20f68053ea5dda5fcbe9975d56
Satyam Bhatia <bhatiasatyam1725@gmail.com>
2020-09-03 15:02:06 +0530

    Update D08P1_LeetCode_268.java

--- 200_LeetCode_Questions/Java/D08P1_LeetCode_268.java
+++ 200_LeetCode_Questions/Java/D08P1_LeetCode_268.java
@@ -1,1 +1,12 @@
-# Java code coming soon
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Missing Number.
+// Memory Usage: 40.3 MB, less than 54.52% of Java online submissions for Missing Number.
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for(int num : nums){
+            sum += num;
+        }
+        return (nums.length * (nums.length + 1) / 2) - sum;
+    }
+}
