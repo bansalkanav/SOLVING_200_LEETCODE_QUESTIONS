f796db742c5bea05fef5d53ac3ef193f5cf94d39
shubha028 <shubhasharma0208@gmail.com>
2020-10-05 20:13:03 +0530

    D19P1_LeetCode_1512solved

--- 200_LeetCode_Questions/Java/D19P1_LeetCode_1512.java
+++ 200_LeetCode_Questions/Java/D19P1_LeetCode_1512.java
@@ -1,1 +1,11 @@
-# Java code coming soon
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+         int ans = 0;
+        for(int i=0; i<nums.length-1; i++) {
+            for(int j=i+1; j<nums.length; j++) {
+                if(nums[i] == nums[j]) ans++;
+            }
+        }
+        return ans;
+    }
+}
