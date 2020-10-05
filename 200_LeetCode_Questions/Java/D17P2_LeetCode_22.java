8e54efc41d9d5c434a26fd30ff7f2b03bbc33ccc
swift1719 <ayushpawar1501@gmail.com>
2020-10-05 15:27:48 +0530

    D17 all problems solution

--- 200_LeetCode_Questions/Java/D17P2_LeetCode_22.java
+++ 200_LeetCode_Questions/Java/D17P2_LeetCode_22.java
@@ -1,1 +1,29 @@
-# Java code coming soon
+class Solution {
+    
+    private void solve(int o,int c,String op,List<String>res)
+    {
+        if(o==0 && c==0)
+        {
+            res.add(op);
+            return;
+        }
+        if(o!=0)
+        {
+            String op1=op+"(";
+            solve(o-1,c,op1,res);
+        }
+        if(o<c)
+        {
+            String op2=op+")";
+            solve(o,c-1,op2,res);
+        }
+    }
+    
+    public List<String> generateParenthesis(int n) {
+        List<String>res=new ArrayList<>();
+        int open=n,close=n;
+        String op="";
+        solve(open,close,op,res);
+        return res;
+    }
+}
--- 200_LeetCode_Questions/Java/D17P3_LeetCode_78.java
+++ 200_LeetCode_Questions/Java/D17P3_LeetCode_78.java
@@ -1,1 +1,31 @@
-# Java code coming soon
+class Solution {
+    List<List<Integer>> lst=new ArrayList<>();
+    
+    public void sets(int start,List<Integer>sub,int nums[],int end)
+    {
+        if(sub.size()>end)
+            return;
+        if(sub.size()==end)
+        {
+            lst.add(new ArrayList<>(sub));
+            return;
+        }
+        
+        for(int i=start;i<nums.length;i++)
+        {
+            sub.add(nums[i]);
+            sets(i+1,sub,nums,end);
+            sub.remove(sub.size()-1);
+        }
+        return;
+    }
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        for(int i=0;i<=nums.length;i++)
+        {
+            sets(0,new ArrayList<Integer>(),nums,i);
+        }
+        return lst;
+    }
+}
