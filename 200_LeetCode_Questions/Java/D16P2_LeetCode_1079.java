07549ca4731266b14eb6752dfbd088f588e5aa5b
swift1719 <ayushpawar1501@gmail.com>
2020-10-05 15:32:23 +0530

    D16 all solutions

--- 200_LeetCode_Questions/Java/D16P2_LeetCode_1079.java
+++ 200_LeetCode_Questions/Java/D16P2_LeetCode_1079.java
@@ -1,1 +1,40 @@
-# Java code coming soon
+class Solution {
+    private int count = 0;
+    
+    private void rec(char arr[],boolean visited[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            if(visited[i])
+            {
+                continue;
+            }
+            
+            count++;
+            visited[i]=true;
+            rec(arr,visited);
+            visited[i]=false;
+            
+            while(i+1<arr.length && arr[i]==arr[i+1])
+            {
+                i++;
+            }
+        }
+    }
+    
+    public int numTilePossibilities(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+        
+        char arr[]=s.toCharArray();
+        
+        boolean visited[]=new boolean[s.length()];
+        
+        Arrays.sort(arr);
+        
+        rec(arr,visited);
+        
+        return count;
+    }
+    
+}
--- 200_LeetCode_Questions/Java/D16P3_LeetCode_1219.java
+++ 200_LeetCode_Questions/Java/D16P3_LeetCode_1219.java
@@ -1,1 +1,35 @@
-# Java code coming soon
+class Solution {
+    public static int solve(int [][]grid,int row,int col)
+    {
+        if(row<0 || col<0 || row==grid.length || col==grid[0].length || grid[row][col]==0)
+            return 0;
+        
+        int temp=grid[row][col];
+        grid[row][col]=0;
+        
+        int up=solve(grid,row-1,col);
+        int down=solve(grid,row+1,col);
+        int left=solve(grid,row,col-1);
+        int right=solve(grid,row,col+1);
+            
+        grid[row][col]=temp;
+        
+        return temp + Math.max(up,Math.max(down,Math.max(left,right)));
+    }
+    public int getMaximumGold(int[][] grid) {
+        int max=0;
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]==0)
+                {
+                    continue;
+                }
+                int value=solve(grid,i,j);
+                max=max>value?max:value;
+            }
+        }
+        return max;
+    }
+}
