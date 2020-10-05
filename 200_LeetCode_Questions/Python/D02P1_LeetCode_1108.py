a5d37a5250cd2940e903809524f49c5928299e0e
Harsh Udai <harshudai2001@gmail.com>
2020-10-05 21:38:37 +0530

    Added the Codes of 1108,1295,1512
    I have added the codes according to the issues, thanks.

--- 200_LeetCode_Questions/Python/D02P1_LeetCode_1108.py
+++ 200_LeetCode_Questions/Python/D02P1_LeetCode_1108.py
@@ -1,1 +1,13 @@
-# Python3 code coming soon
+# Python3 ,By :- Harsh Udai
+# Que. Defanging an IP Address
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        str1=""
+        for i in range(0,len(address)):
+            if(address[i]=='.'):
+                str1=str1+'[.]'
+            else:
+                str1=str1+address[i]
+                
+        return str1
--- PractiseLeetCode/Python/SquaresofaSortedArray.py
+++
@@ -1,7 +0,0 @@
-# Given an array of integers A sorted in non-decreasing order, 
-# return an array of the squares of each number, also in sorted non-decreasing order.
-
-class Solution:
-    def sortedSquares(self, A: List[int]) -> List[int]:
-        l1=[(i**2) for i in A]
-        return(sorted(l1))
