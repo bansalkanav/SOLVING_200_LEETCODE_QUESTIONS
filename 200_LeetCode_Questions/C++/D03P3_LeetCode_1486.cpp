26ff9a2830768bb412f6542be9d15f70e6e7c8bf
shailesh singh <yaiamshailesh@gmail.com>
2020-10-06 11:14:44 +0530

    Update D03P3_LeetCode_1486.cpp

--- 200_LeetCode_Questions/C++/D03P3_LeetCode_1486.cpp
+++ 200_LeetCode_Questions/C++/D03P3_LeetCode_1486.cpp
@@ -1,1 +1,12 @@
 # C++ code coming soon
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int x=0;
+        while(n--){
+            x^=start;
+            start+=2;
+        }
+        return x;
+    }
+};
