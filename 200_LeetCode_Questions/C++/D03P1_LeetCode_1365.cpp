3f7d1aa5c7c66a9ebf63b871ee1e02a022c19521
Sandeep Kumar Srivastava <56189781+Sandeep228@users.noreply.github.com>
2020-10-06 11:43:06 +0530

    Update D03P1_LeetCode_1365.cpp

--- 200_LeetCode_Questions/C++/D03P1_LeetCode_1365.cpp
+++ 200_LeetCode_Questions/C++/D03P1_LeetCode_1365.cpp
@@ -1,1 +1,22 @@
-# C++ code coming soon
+class Solution {
+public:
+vector smallerNumbersThanCurrent(vector& nums)
+{
+vector nums1{nums};
+int count = 0;
+for( int i = 0; i < nums.size();i++)
+{
+for ( int j = 0; j < nums.size(); j ++)
+{
+if((nums[i]>nums[j]) && j != i )
+{
+count++;
+}
+}
+nums1[i] = count;
+count=0;
+}
+
+    return nums1;
+}
+};
