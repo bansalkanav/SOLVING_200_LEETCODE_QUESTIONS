489461e62ecc93884abfffebc0c77213301e9fd4
Priyanshu Parihar <55618725+Priyanshuparihar@users.noreply.github.com>
2020-10-06 01:10:59 -0700

    Update D03P2_LeetCode_1281.py

--- 200_LeetCode_Questions/Python/D03P2_LeetCode_1281.py
+++ 200_LeetCode_Questions/Python/D03P2_LeetCode_1281.py
@@ -1,1 +1,12 @@
-# Python3 code coming soon
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        a=n
+        mul=1
+        su=0
+        while(a>0):
+            rem=a%10
+            mul*=rem
+            su+=rem
+            a=a//10
+        return mul-su
+        
