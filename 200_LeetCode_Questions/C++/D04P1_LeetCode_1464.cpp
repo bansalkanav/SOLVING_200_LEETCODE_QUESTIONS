769186bcf27fb32583b941c0bec18ab62627724a
Rohit-varma <44708807+Rohit-varma@users.noreply.github.com>
2020-10-06 12:13:53 +0530

    Update D04P1_LeetCode_1464.cpp
    added code for Maximum Product of Two Elements in an Array

--- 200_LeetCode_Questions/C++/D04P1_LeetCode_1464.cpp
+++ 200_LeetCode_Questions/C++/D04P1_LeetCode_1464.cpp
@@ -1,1 +1,29 @@
-# C++ code coming soon
+//Maximum Product of Two Elements in an Array
+
+//Question: Given the array of integers nums, you will choose two different indices i and j of that array. 
+// Return the maximum value of (nums[i]-1)*(nums[j]-1)
+
+//size of the given array will be >=2
+
+//Solution: helper function
+class Solution 
+{
+public:
+	
+    int maxProduct(vector<int>& nums) 
+	{
+	        
+		int max1 = INT_MIN, max2 = INT_MIN;
+		for(int i=0;i<nums.size();i++)
+		{
+			if(max1<nums[i])
+			{
+				max2 = max1;
+				max1 = nums[i];
+			}
+			else if(max2<nums[i])
+				max2 = nums[i];
+		}
+		return (max1-1)*(max2-1);
+	}
+};
