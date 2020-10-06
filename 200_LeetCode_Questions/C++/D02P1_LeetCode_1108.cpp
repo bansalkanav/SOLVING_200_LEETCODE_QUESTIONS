896b2126664eb118fe4b963bd1d922fe1013c179
padmaja-22 <pskbuggaveeti@gmail.com>
2020-10-06 09:44:38 +0530

    Uploaded solutions

--- 200_LeetCode_Questions/C++/D02P1_LeetCode_1108.cpp
+++ 200_LeetCode_Questions/C++/D02P1_LeetCode_1108.cpp
@@ -1,1 +1,16 @@
-# C++ code coming soon
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string new_string="";
+        for(int i=0;i<address.size();i++){
+            if(address[i]=='.'){
+                new_string.append("[.]");
+            }
+            else{
+                new_string=new_string+address[i];
+            }
+        }
+        return new_string;
+        
+    }
+};
--- 200_LeetCode_Questions/C++/D02P3_LeetCode_1512.cpp
+++ 200_LeetCode_Questions/C++/D02P3_LeetCode_1512.cpp
@@ -1,1 +1,13 @@
-# C++ code coming soon
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+                if(i!=j && nums[i]==nums[j] && i<j)count++;
+            }
+        }
+        return count;
+        
+    }
+};
