aab4a29673e6941f18718203b63c00063b3e068a
KARTIK PUROHIT <58941118+darkworgnkp@users.noreply.github.com>
2020-10-06 09:54:36 +0530

    Update D02P1_LeetCode_1108.cpp
    LeetCode Problem 1108 : Defanging an IP Address

--- 200_LeetCode_Questions/C++/D02P1_LeetCode_1108.cpp
+++ 200_LeetCode_Questions/C++/D02P1_LeetCode_1108.cpp
@@ -1,1 +1,29 @@
-# C++ code coming soon
+// Problem :  Defanging an IP Address
+
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+// A defanged IP address replaces every period "." with "[.]".
+
+// Example 1:
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+  
+// Solution : 
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+       string new_address;
+        string p;
+        int n = address.size();
+    for(int i = 0;i<n;i++){
+        p = address[i];
+        if (p == "."){
+            new_address.append("[.]");
+        }
+        else{
+            new_address.append(p);
+        }
+    }
+        return new_address;
+    }
+};
