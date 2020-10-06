14b921d91fe91ead738a8a91190c3a7ac2b4dbb2
ahthserhsluk <52717339+ahthserhsluk@users.noreply.github.com>
2020-10-06 14:38:52 +0530

    Update D03P3_LeetCode_1486.java

--- 200_LeetCode_Questions/Java/D03P3_LeetCode_1486.java
+++ 200_LeetCode_Questions/Java/D03P3_LeetCode_1486.java
@@ -1,12 +1,27 @@
-// Runtime: 0 ms, faster than 100.00% of Java online submissions for XOR Operation in an Array.
-// Memory Usage: 36.4 MB, less than 55.35% of Java online submissions for XOR Operation in an Array.
-
-class Solution {
-    public int xorOperation(int n, int start) {
-        int xor = 0;
-        for(int i = 0; i < n; i++){
-            xor ^= (start + (2 * i));
+class alpha { 
+      
+   
+    static int xorOfArray(int arr[], int n) 
+    { 
+        int xor_arr = 0; 
+      
+     for (int i = 0; i < n; i++) { 
+      
+          
+            xor_arr = xor_arr ^ arr[i]; 
         }
-        return xor;
-    }
-}
+        return xor_arr; 
+    } 
+      
+   
+    public static void main (String[] args) 
+    { 
+      
+        int arr[] = { 3, 9, 12, 13, 15 }; 
+        int n = arr.length; 
+      
+        // Function call 
+        System.out.println(xorOfArray(arr, n)); 
+  
+    } 
+} 
