6e6363d37f0092d2adf315d0f4965757e3d4db59
Priyanshu Parihar <55618725+Priyanshuparihar@users.noreply.github.com>
2020-10-06 00:07:30 -0700

    Update D05P1_LeetCode_1342.py

--- 200_LeetCode_Questions/Python/D05P1_LeetCode_1342.py
+++ 200_LeetCode_Questions/Python/D05P1_LeetCode_1342.py
@@ -1,1 +1,11 @@
 # Python3 code coming soon
+class Solution:
+    def numberOfSteps (self, num: int) -> int:
+        count=0
+        while num>0:
+            count+=1
+            if num%2==0:
+                num=num/2
+            else:
+                num=num-1
+        return count
