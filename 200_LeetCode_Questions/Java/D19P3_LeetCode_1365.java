d08ef460ec1e911094db8fc54afb33c56e4a9b7c
swift1719 <ayushpawar1501@gmail.com>
2020-10-06 23:47:27 +0530

    D19 all solutions

--- 200_LeetCode_Questions/Java/D19P3_LeetCode_1365.java
+++ 200_LeetCode_Questions/Java/D19P3_LeetCode_1365.java
@@ -1,1 +1,26 @@
-# Java code coming soon
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int count[]=new int[101];
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            count[nums[i]]++;
+        }
+        for(int i=1;i<101;i++)
+        {
+            count[i]+=count[i-1];
+        }
+        
+        int res[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]<1)
+            {
+                res[i]=0;
+                continue;
+            }
+            res[i]=count[nums[i]-1];                
+        }
+        return res;
+    }
+}
