e515ee57fa106ac12cd4ed4dfa82c49eefad33ee
shailesh singh <yaiamshailesh@gmail.com>
2020-10-06 11:14:09 +0530

    Update D03P2_LeetCode_1281.cpp

--- 200_LeetCode_Questions/C++/D03P2_LeetCode_1281.cpp
+++ 200_LeetCode_Questions/C++/D03P2_LeetCode_1281.cpp
@@ -1,1 +1,15 @@
 # C++ code coming soon
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int product=1;
+        int sum=0;
+        while(n>0){
+            int temp=n%10;
+            n/=10;
+            product*=temp;
+            sum+=temp;
+        }
+        return product-sum;
+    }
+};
