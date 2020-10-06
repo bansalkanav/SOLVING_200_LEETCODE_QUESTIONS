0befe5acdb0020aa6b2de6045af4e0742932810c
KARTIK PUROHIT <58941118+darkworgnkp@users.noreply.github.com>
2020-10-06 10:05:06 +0530

    Update D02P3_LeetCode_1512.cpp
    LeetCode Problem 1512 : Number of Good Pairs.

--- 200_LeetCode_Questions/C++/D02P3_LeetCode_1512.cpp
+++ 200_LeetCode_Questions/C++/D02P3_LeetCode_1512.cpp
@@ -1,1 +1,32 @@
-# C++ code coming soon
+// Problem : Number of Good Pairs
+
+// Given an array of integers nums.
+// A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+// Return the number of good pairs.
+  
+// Example 1:
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+// Solution :
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums)
+    {   ios_base::sync_with_stdio(false); 
+        cin.tie(0);
+        cout.tie(0);
+        int count_pair=0;
+        int p = nums.size();
+        for(int i=0;i<p;i++)
+        {
+            for(int j=0;j<p;j++)
+            {
+                if(i!=j && nums[i]==nums[j] && i<j)
+                    count_pair++;
+            }
+        }
+        return count_pair; 
+    }
+};
