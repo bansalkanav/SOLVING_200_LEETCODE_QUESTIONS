a2e2fee0b86d3d5b143503ae896b71b0760533e1
Dheeraj Bhagchandani <54698983+Dheeraj-01@users.noreply.github.com>
2020-10-06 18:58:23 +0530

    Update D05P3_LeetCode_136.cpp
    Added 136. Single Number

--- 200_LeetCode_Questions/C++/D05P3_LeetCode_136.cpp
+++ 200_LeetCode_Questions/C++/D05P3_LeetCode_136.cpp
@@ -1,1 +1,9 @@
-# C++ code coming soon
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int i : nums)
+            ans ^= i;
+        return ans;
+    }
+};
