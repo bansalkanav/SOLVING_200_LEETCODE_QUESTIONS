8517536433b698d78bd3da7be63938bfe974e292
Priyanshu Parihar <55618725+Priyanshuparihar@users.noreply.github.com>
2020-10-06 18:44:33 +0530

    Update D04P3_LeetCode_1450.py

--- 200_LeetCode_Questions/Python/D04P3_LeetCode_1450.py
+++ 200_LeetCode_Questions/Python/D04P3_LeetCode_1450.py
@@ -1,1 +1,10 @@
-# Python3 code coming soon
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        count=0
+        j=0
+        while j<len(startTime):
+            for i in range(startTime[j],endTime[j]+1):
+                if i==queryTime:
+                    count+=1
+            j+=1
+        return count
