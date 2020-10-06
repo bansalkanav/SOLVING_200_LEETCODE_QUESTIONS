e5899af031af49170155f0b3fe6d2128b456a8a2
Raghavendra Kurdekar <53779288+iamraghs@users.noreply.github.com>
2020-10-06 14:02:41 +0530

    Update D03P3_LeetCode_1486.py

--- 200_LeetCode_Questions/Python/D03P3_LeetCode_1486.py
+++ 200_LeetCode_Questions/Python/D03P3_LeetCode_1486.py
@@ -1,1 +1,10 @@
 # Python3 code coming soon
+
+
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        ans = temp = start
+        for i in range(1, n):
+            temp += 2  
+            ans = ans ^ temp
+        return ans
