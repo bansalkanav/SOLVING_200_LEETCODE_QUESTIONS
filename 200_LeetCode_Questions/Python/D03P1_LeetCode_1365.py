e69c3a2a1e36e3b402e42f0fa89ebf2da8182c30
Priyanshu Parihar <55618725+Priyanshuparihar@users.noreply.github.com>
2020-10-06 01:13:21 -0700

    Update D03P1_LeetCode_1365.py

--- 200_LeetCode_Questions/Python/D03P1_LeetCode_1365.py
+++ 200_LeetCode_Questions/Python/D03P1_LeetCode_1365.py
@@ -1,1 +1,11 @@
-# Python3 code coming soon
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        l=[]
+        
+        for i in range(len(nums)):
+            count=0
+            for j in range(len(nums)):
+                if nums[i]>nums[j]:
+                    count+=1
+            l.append(count)
+        return l
