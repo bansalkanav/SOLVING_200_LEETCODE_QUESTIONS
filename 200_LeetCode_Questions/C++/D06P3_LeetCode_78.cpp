3264f3caa4402231a48f47d63ed76db3db744351
KARTIK PUROHIT <58941118+darkworgnkp@users.noreply.github.com>
2020-10-07 12:01:27 +0530

    Update D06P3_LeetCode_78.cpp
    LeetCode Problem 78 : Subsets.

--- 200_LeetCode_Questions/C++/D06P3_LeetCode_78.cpp
+++ 200_LeetCode_Questions/C++/D06P3_LeetCode_78.cpp
@@ -1,1 +1,45 @@
-# C++ code coming soon
+// Problem : Subsets
+
+// Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+// Note: The solution set must not contain duplicate subsets.
+
+// Example:
+
+// Input: nums = [1,2,3]
+// Output:
+// [
+//   [3],
+//   [1],
+//   [2],
+//   [1,2,3],
+//   [1,3],
+//   [2,3],
+//   [1,2],
+//   []
+// ]
+
+// Solution : 
+
+class Solution
+{
+public:
+    vector<vector<int>> subsets(vector<int>& nums)
+    {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        int set_size=nums.size();
+        int n=pow(2,set_size);
+        for(int i=0;i<n;i++)
+        {
+            vector<int> t;
+            for(int j=0;j<=set_size;j++)
+            {
+                if((1<<j)&(i))
+                t.push_back(nums[j]);
+            }
+            res.push_back(t);
+        }
+        return res;  
+    }
+};
