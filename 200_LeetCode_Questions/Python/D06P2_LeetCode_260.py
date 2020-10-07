22d64f373333da4267cdef7e6911b2244922da40
Priyanshu Parihar <55618725+Priyanshuparihar@users.noreply.github.com>
2020-10-06 23:01:32 -0700

    Update D06P2_LeetCode_260.py

--- 200_LeetCode_Questions/Python/D06P2_LeetCode_260.py
+++ 200_LeetCode_Questions/Python/D06P2_LeetCode_260.py
@@ -1,1 +1,8 @@
-# Python3 code coming soon
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        l=[]
+        for i in nums:
+            if nums.count(i)==1:
+                l.append(i)
+        return l
+        
