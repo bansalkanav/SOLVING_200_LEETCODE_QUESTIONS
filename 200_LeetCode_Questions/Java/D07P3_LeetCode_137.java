c9ca6090eb14cda8f120fd3f64b9b758c76273bf
Mohit Bhagchandani <72273848+MohitBhagchandani0601@users.noreply.github.com>
2020-10-07 22:25:55 +0530

    Update D07P3_LeetCode_137.java
    Added 137. Single Number II

--- 200_LeetCode_Questions/Java/D07P3_LeetCode_137.java
+++ 200_LeetCode_Questions/Java/D07P3_LeetCode_137.java
@@ -1,1 +1,21 @@
-# Java code coming soon
+class Solution {
+    public int singleNumber(int[] nums) {
+       int[] bitCounts = new int[32];
+       for (int i = 0; i < nums.length; i++) {
+            int j=0;
+            int num = nums[i];
+            while(j<bitCounts.length){
+                bitCounts[j]+=num&1;
+                num=num>>1;
+                j++;
+            }
+       }
+       int ans=0;
+       for (int i = bitCounts.length-1; i>=0; i--) {
+           ans+=bitCounts[i]%3;
+           if(i>0)ans=ans<<1;
+       }
+       
+       return ans;
+    }
+}
