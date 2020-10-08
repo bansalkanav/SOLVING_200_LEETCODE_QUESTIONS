c9522ff6577c91d8cc4ec026211a253f227f4ac2
Priyanshu Parihar <55618725+Priyanshuparihar@users.noreply.github.com>
2020-10-07 22:40:01 -0700

    Update D07P1_LeetCode_389.py

--- 200_LeetCode_Questions/Python/D07P1_LeetCode_389.py
+++ 200_LeetCode_Questions/Python/D07P1_LeetCode_389.py
@@ -1,1 +1,8 @@
-# Python3 code coming soon
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        l=[]
+        for i in t:
+            l.append(i)
+        for i in s:
+            l.remove(i)
+        return l[0]
