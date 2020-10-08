802fec4772dff382dc75f484bca450fd7a75c69e
Priyanshu Parihar <55618725+Priyanshuparihar@users.noreply.github.com>
2020-10-07 22:42:36 -0700

    Update D07P2_LeetCode_461.py

--- 200_LeetCode_Questions/Python/D07P2_LeetCode_461.py
+++ 200_LeetCode_Questions/Python/D07P2_LeetCode_461.py
@@ -1,1 +1,9 @@
-# Python3 code coming soon
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        a=x^y
+        count=0
+        while(a>0):
+            if a%2!=0:
+                count+=1
+            a=a>>1
+        return count
