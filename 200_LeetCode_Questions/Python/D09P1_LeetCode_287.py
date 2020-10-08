6174f84b5fb6160057590885b06919af761d56c5
Priyanshu Parihar <55618725+Priyanshuparihar@users.noreply.github.com>
2020-10-08 12:06:19 +0530

    Update D09P1_LeetCode_287.py

--- 200_LeetCode_Questions/Python/D09P1_LeetCode_287.py
+++ 200_LeetCode_Questions/Python/D09P1_LeetCode_287.py
@@ -1,1 +1,6 @@
-# Python3 code coming soon
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i  in range(len(nums)):
+            if nums[i]==nums[i+1]:
+                return nums[i]
