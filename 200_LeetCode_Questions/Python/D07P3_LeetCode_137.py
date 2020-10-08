936f6367fc25788510ec1117d195ce99f5fb3ce6
Priyanshu Parihar <55618725+Priyanshuparihar@users.noreply.github.com>
2020-10-07 22:43:48 -0700

    Update D07P3_LeetCode_137.py

--- 200_LeetCode_Questions/Python/D07P3_LeetCode_137.py
+++ 200_LeetCode_Questions/Python/D07P3_LeetCode_137.py
@@ -1,1 +1,6 @@
-# Python3 code coming soon
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for i in nums:
+            if(nums.count(i)==1):
+                return i
+      
