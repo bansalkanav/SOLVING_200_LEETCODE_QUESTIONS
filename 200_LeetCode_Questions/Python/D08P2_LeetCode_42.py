4c030b4c03f018738322ad73e7539154e64e421f
Dom <xblgator@live.com>
2020-10-08 12:34:23 +0900

    leetcode day8 python

--- 200_LeetCode_Questions/Python/D08P2_LeetCode_42.py
+++ 200_LeetCode_Questions/Python/D08P2_LeetCode_42.py
@@ -1,1 +1,28 @@
-# Python3 code coming soon
+#Given n non-negative integers representing an elevation map where the width of each bar is 1, 
+#compute how much water it is able to trap after raining.
+
+class Solution:
+  def trap(self, height: List[int]) -> int:
+      
+      water = 0
+      index = 0
+      stack = []
+      
+      while index < len(height):
+          while len(stack) != 0 and height[index] > height[stack[len(stack) - 1]]:
+              top = stack.pop()
+              
+              if len(stack) == 0:
+                  break
+              
+              width = index - stack[len(stack) - 1] - 1
+              min_height = min(height[index], height[stack[len(stack) - 1]])
+              min_height -= height[top]
+              
+              area = width * min_height
+              water += area
+          
+          stack.append(index)
+          index += 1
+      
+      return water
--- 200_LeetCode_Questions/Python/D08P3_LeetCode_53.py
+++ 200_LeetCode_Questions/Python/D08P3_LeetCode_53.py
@@ -1,1 +1,14 @@
-# Python3 code coming soon
+#Given an integer array nums, find the contiguous subarray (containing at least one number) 
+#which has the largest sum and return its sum
+
+class Solution:
+  #Kadane's Algorithm
+  def maxSubArray(self, nums: List[int]) -> int:
+      max_sum = nums[0]
+      cur_sum = max_sum
+      
+      for i in range(1, len(nums)):
+          cur_sum = max(nums[i] + cur_sum, nums[i])
+          max_sum = max(cur_sum, max_sum)
+      
+      return max_sum
