93adb417ad733ee314df9a4444cca901034164ad
Priyanshu Parihar <55618725+Priyanshuparihar@users.noreply.github.com>
2020-10-08 12:07:44 +0530

    Update D09P2_LeetCode_1200.py

--- 200_LeetCode_Questions/Python/D09P2_LeetCode_1200.py
+++ 200_LeetCode_Questions/Python/D09P2_LeetCode_1200.py
@@ -1,1 +1,18 @@
-# Python3 code coming soon
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        l=[]
+        arr.sort()
+        for i in arr:
+            for j in arr:
+                if i-j>0:
+                    l.append(i-j)
+        m=min(l)
+        l=[]
+        for i in arr:
+            for j in arr:
+                if i-j==m:
+                    l.append([j,i])
+        return l
+
+
+        
