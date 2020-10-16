cece042e168f13fe1cd05133badf1084b05519e5
Luiza Carneiro <luizacarne@gmail.com>
2020-10-15 22:22:11 -0700

    LeetCode287

--- 200_LeetCode_Questions/Java/D09P1_LeetCode_287.java
+++ 200_LeetCode_Questions/Java/D09P1_LeetCode_287.java
@@ -1,1 +1,19 @@
-# Java code coming soon
+// https://leetcode.com/problems/find-the-duplicate-number/
+// Runtime: 2 ms, faster than 50.06% of Java online submissions for Find the Duplicate Number.
+// Memory Usage: 39.9 MB, less than 15.35% of Java online submissions for Find the Duplicate Number.
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++){
+            if (set.contains(nums[i])){
+                return nums[i];
+            }
+            set.add(nums[i]);
+        }
+        return 0;
+    }
+}
