568a7b78954a29513aef9ad14625579211a3e379
parikshitsen <73110836+parikshitsen@users.noreply.github.com>
2020-10-19 17:32:30 +0530

    Update D11P2_LeetCode_153.py

--- 200_LeetCode_Questions/Python/D11P2_LeetCode_153.py
+++ 200_LeetCode_Questions/Python/D11P2_LeetCode_153.py
@@ -1,3 +1,15 @@
 class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-         return nums.index(max(nums))
+    def findMin(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while (left < right):
+            midpoint = int(left + (right - left)/2)
+            if(nums[midpoint] < nums[midpoint -1]):
+                return nums[midpoint]
+            elif (nums[midpoint] >= nums[left] and nums[midpoint] > nums[right]):
+                left = midpoint+1
+            else:
+                right = midpoint - 1
+
+        return nums[left]
