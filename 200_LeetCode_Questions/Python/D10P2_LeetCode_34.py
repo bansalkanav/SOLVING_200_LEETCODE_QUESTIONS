bc8a903a5995ed3147085863391db3ddbbb4f660
Rohan Gupta <rg300557@gmail.com>
2020-10-19 16:35:12 +0530

    Added Day 10 LeetCode solutions

--- 200_LeetCode_Questions/Python/D10P2_LeetCode_34.py
+++ 200_LeetCode_Questions/Python/D10P2_LeetCode_34.py
@@ -1,1 +1,21 @@
-# Python3 code coming soon
+# Find First and Last Position of Element in Sorted Array
+class Solution(object):
+    def searchRange(self, nums, target):
+        if len(nums) == 0: 
+            return([-1, -1])
+        low_element, high_element = 0, len(nums)
+        while low_element < high_element:
+            mid_element = (high_element + low_element) // 2
+            if nums[mid_element] == target: 
+                left_element, right_element = mid_element, mid_element
+                while (left_element-1) >= 0 and nums[left_element-1] == target: 
+                    left_element -= 1
+                while (right_element+1) < len(nums) and nums[right_element+1] == target: 
+                    right_element += 1
+                return([left_element, right_element])
+                break
+            elif nums[mid_element] < target: 
+                low_element = mid_element + 1
+            elif nums[mid_element] > target: 
+                high_element = mid_element
+        return([-1, -1])
--- 200_LeetCode_Questions/Python/D10P3_LeetCode_69.py
+++ 200_LeetCode_Questions/Python/D10P3_LeetCode_69.py
@@ -1,1 +1,7 @@
-# Python3 code coming soon
+# Sqrt(x)
+class Solution(object):
+    def mySqrt(self, x):
+        # return(int(x ** 0.5))
+        import math
+        return(int(math.sqrt(x)))
+        
