88c00a86c4bb40cb69bc3ef3157c819e38e4a6b4
parikshitsen <73110836+parikshitsen@users.noreply.github.com>
2020-10-19 17:35:09 +0530

    Update D11P3_LeetCode_33.py

--- 200_LeetCode_Questions/Python/D11P3_LeetCode_33.py
+++ 200_LeetCode_Questions/Python/D11P3_LeetCode_33.py
@@ -1,1 +1,21 @@
-# Python3 code coming soon
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        def bsearch(nums, target, lo, hi):
+            if lo == hi:
+                return lo if nums[lo] == target else -1
+
+            mid = (lo + hi) // 2
+            if nums[mid] == target:
+                return mid
+            
+            if (nums[hi] > nums[mid] and (target >= nums[mid] and target <= nums[hi])) or (nums[hi] < nums[mid] and (target <= nums[hi] or target >= nums[mid])):
+                return bsearch(nums, target, mid + 1, hi)
+            else:
+                return bsearch(nums, target, lo, mid)
+            
+        return bsearch(nums, target, 0, len(nums) - 1)
