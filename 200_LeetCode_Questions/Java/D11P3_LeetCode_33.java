3a0cff422acb64834c92da17952222e0d55a575e
Ritanshu Kumar Singh <ritanshukumarsingh8922@gmail.com>
2020-10-21 01:21:37 +0530

    search in rotated sorted array solved

--- 200_LeetCode_Questions/Java/D11P3_LeetCode_33.java
+++ 200_LeetCode_Questions/Java/D11P3_LeetCode_33.java
@@ -1,1 +1,18 @@
 # Java code coming soon
+class Solution {
+    public int search(int[] nums, int target) {
+         //If array is empty, return -1
+        if(nums.length == 0) {
+           return -1;
+        }
+        //Traverse an array
+        for(int i = 0; i < nums.length; i++) {
+ 
+            //If element is found, then return it's index
+            if(nums[i] == target) {
+                 return i;
+            }
+         }
+         return -1;
+    }
+}
