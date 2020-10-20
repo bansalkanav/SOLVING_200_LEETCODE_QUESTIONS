97dfda67f99e87d3d81c571abf4220b1dee53377
Ritanshu Kumar Singh <ritanshukumarsingh8922@gmail.com>
2020-10-21 01:35:47 +0530

    find peak element issue #100 fixed

--- 200_LeetCode_Questions/Java/D11P3_LeetCode_33.java
+++ 200_LeetCode_Questions/Java/D11P3_LeetCode_33.java
@@ -1,1 +1,16 @@
 # Java code coming soon
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left<right){
+            int mid = left + (right-left)/2;
+            if(nums[mid]<nums[mid+1]){
+                left = mid+1;
+            }else{
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
