f473a30b061a2070cc2eec45e8f05d403ce8992c
Pardeep Kumar <pardeepmalhi816@gmail.com>
2020-10-20 22:18:01 +0500

    Solve the problem of day 10 (problem no.2). LeetCode no. 34

--- 200_LeetCode_Questions/Java/D10P2_LeetCode_34.java
+++ 200_LeetCode_Questions/Java/D10P2_LeetCode_34.java
@@ -1,1 +1,43 @@
-# Java code coming soon
+import java.util.Scanner;
+
+public class D10P2_LeetCode_34 {
+    public static void main(String args []){
+        Scanner s = new Scanner(System.in);
+
+        int i,j,target,elements;
+        int arrpos [] = new int [2];
+
+        System.out.print("input the number of elements ");
+        elements = s.nextInt();
+
+        int grid [] = new int [elements];
+        System.out.println("Input elements in array in ascending order");
+        for (i=0; i<elements; i++){
+            grid[i] = s.nextInt();
+        }
+
+        System.out.print("input the target value ");
+        target = s.nextInt();
+
+        for (i=0; i<elements; i++){
+            if(grid[i] == target){
+                arrpos[0] = i;
+                break;
+            }
+            else
+             arrpos[0] = -1;
+        }
+
+        for (j=i; j<elements; j++){
+            if(grid[j] == target){
+                arrpos[1] = j;
+            }
+            else
+              arrpos[1] = -1;
+        }
+
+        System.out.println("First and last position is ["+arrpos[0]+","+arrpos[1]+"]");
+
+    }
+
+}
