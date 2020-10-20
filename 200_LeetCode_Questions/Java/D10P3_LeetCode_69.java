73712200686defd241dee282896bd45b4c1d05b3
Pardeep Kumar <pardeepmalhi816@gmail.com>
2020-10-20 22:30:58 +0500

    Solved the problem of day 10 (problem no. 3). LeetCode no. 69

--- 200_LeetCode_Questions/Java/D10P3_LeetCode_69.java
+++ 200_LeetCode_Questions/Java/D10P3_LeetCode_69.java
@@ -1,1 +1,12 @@
-# Java code coming soon
+import java.util.Scanner;
+public class D10P3_LeetCode_69{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Input a non-negative number : ");
+        int num = s.nextInt();
+
+        int squrt = (int) Math.sqrt(num);
+
+        System.out.println("The square root of "+num+" = "+squrt);
+    }
+}
