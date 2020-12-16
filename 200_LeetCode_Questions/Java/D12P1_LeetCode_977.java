d074f5f5955ef5f047f020dbf41d6aa27d5ddd4d
Ismail Ou Khalek <ismailhivi@gmail.com>
2020-12-16 21:59:00 +0100

    D12P1_LeetCode_977 Solution has been added

--- 200_LeetCode_Questions/Java/D12P1_LeetCode_977.java
+++ 200_LeetCode_Questions/Java/D12P1_LeetCode_977.java
@@ -1,1 +1,22 @@
-# Java code coming soon
+/*
+Runtime: 1 ms, faster than 100.00% of Java online submissions for Squares of a Sorted Array.
+Memory Usage: 40.8 MB, less than 83.69% of Java online submissions for Squares of a Sorted Array.
+*/
+
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        int[] returnedArray = new int[A.length];
+        int right = A.length-1;
+        int left = 0;
+        for(int i=0 ; i < A.length ; i++){
+            if(Math.abs(A[left])<Math.abs(A[right])){
+                returnedArray[A.length-i-1] = A[right]*A[right];
+                right--;
+            }else{
+                returnedArray[A.length-i-1] = A[left]*A[left];
+                left++;
+            }
+        }
+        return returnedArray;
+    }
+}
