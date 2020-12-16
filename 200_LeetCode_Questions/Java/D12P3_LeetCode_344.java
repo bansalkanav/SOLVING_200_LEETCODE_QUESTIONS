982082e91b212c9202207fcfc96e9bbe8de6a2d1
Ismail Ou Khalek <ismailhivi@gmail.com>
2020-12-16 22:01:10 +0100

    D12P3_LeetCode_344 Solution has been added

--- 200_LeetCode_Questions/Java/D12P3_LeetCode_344.java
+++ 200_LeetCode_Questions/Java/D12P3_LeetCode_344.java
@@ -1,1 +1,15 @@
-# Java code coming soon
+/*
+Runtime: 1 ms, faster than 95.19% of Java online submissions for Reverse String.
+Memory Usage: 45.5 MB, less than 94.24% of Java online submissions for Reverse String.
+*/
+
+class Solution {
+    public void reverseString(char[] s) {
+        char hold;
+        for(int i = 0 ; i < s.length/2 ; i++){
+            hold = s[i];
+            s[i] = s[s.length - i - 1];
+            s[s.length - i - 1] = hold;
+        }
+    }
+}
