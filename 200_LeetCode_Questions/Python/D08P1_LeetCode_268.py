c31c2e0cf5e4c3fbba59bd29bf343418eb760d5d
Pranav Bhawsar <137757814+Pranavbh1@users.noreply.github.com>
2023-10-03 22:04:39 +0530

    Update D08P1_LeetCode_268.py

--- 200_LeetCode_Questions/Python/D08P1_LeetCode_268.py
+++ 200_LeetCode_Questions/Python/D08P1_LeetCode_268.py
@@ -1,13 +1,11 @@
 #Given an array nums containing n distinct numbers in the range [0, n], 
 #return the only number in the range that is missing from the array.
 
-class Solution:
-  def missingNumber(self, nums: List[int]) -> int:
-      real_sum = 0
-      missing_sum = 0
-      
-      for i, num in enumerate(nums): 
-          real_sum += i + 1
-          missing_sum += num
-      
-      return real_sum - missing_sum
+x = list(map(int,input().strip().split(',')))
+a = np.array(x)
+print("Input:" , np.unique(a))
+z = []
+for i in range(0,len(a)+1):
+    if i not in x :
+        z.append(i)
+print("Missing number :", z)
