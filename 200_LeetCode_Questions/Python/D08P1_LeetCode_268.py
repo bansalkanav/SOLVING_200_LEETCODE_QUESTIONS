031496bfed3f004baa89f38120197b4ce57dbc17
Pranav Bhawsar <137757814+Pranavbh1@users.noreply.github.com>
2023-10-04 11:43:40 +0530

    Update D08P1_LeetCode_268.py

--- 200_LeetCode_Questions/Python/D08P1_LeetCode_268.py
+++ 200_LeetCode_Questions/Python/D08P1_LeetCode_268.py
@@ -1,11 +1,11 @@
 #Given an array nums containing n distinct numbers in the range [0, n], 
 #return the only number in the range that is missing from the array.
 
-x = list(map(int,input().strip().split(',')))
-a = np.array(x)
-print("Input:" , np.unique(a))
-z = []
-for i in range(0,len(a)+1):
-    if i not in x :
-        z.append(i)
-print("Missing number :", z)
+#Robust way 
+#Sum of range [0,n] - sum of given list                #substraction
+num_list = list(map(int, input().split(",")))
+n = len(num_list)
+sum_n = []
+for i in num_list:
+    sum_n.append(i)
+print((n*(n+1)//2)-sum(sum_n))
